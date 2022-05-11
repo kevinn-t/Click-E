26073d317872d12e4daea39438dc9fa377f2bd04
Kevin Tan <67160289+kevinn-t@users.noreply.github.com>
2022-05-11 09:31:04 -0700

    still duno how to commit with cmd prompt

--- clicke.py
+++ clicke.py
@@ -20,10 +20,6 @@ keyboardApp = Tk()
 keyboardApp.title('Click-E')
 keyboardApp.geometry('813x250+550+370')
 
-run = True
-def stop():
-    run = False
-kill = Button(keyboardApp, text = 'End', width = 5, relief = 'ridge', padx = 4, pady = 4, command = stop).grid(row = 6, column = 11)
 
 #making an entry
 entry = Entry(keyboardApp, width = 85)
@@ -37,12 +33,12 @@ def select(value):
 
 #hold / click
 def spam():
-    while run:
+    while True:
         for ele in inputedKeys:
             pag.press(ele)
     inputedKeys.clear()
 def hold():
-    while run:
+    while True:
         for ele in inputedKeys:
             pag.keyDown(ele)
     inputedKeys.clear()
@@ -51,19 +47,19 @@ spamClick = Button(keyboardApp, text = 'Spam', width = 5, relief = 'ridge', padx
 hold = Button(keyboardApp, text = 'Hold', width = 5, relief = 'ridge', padx = 4, pady = 4, command = hold).grid(column = 13, row = 0)
 
 #keyboard creation
-keys = ['`','1','2','3','4','5','6','7','8','9','-','=','backspace',
-		'tab','q','w','e','r','t','y','u','i','o','p','[',']','\\',
-		'capslock','a','s','d','f','g','h','j','k','l',';','\'','enter',
-		'shiftleft','z','x','c','v','b','n','m',',','.','.','/','shiftright',
-		'controlleft', 'altleft', 'space', 'altright', 'controlright']
+keyboard = ['`','1','2','3','4','5','6','7','8','9','-','=','backspace',
+            'tab','q','w','e','r','t','y','u','i','o','p','[',']','\\',
+            'capslock','a','s','d','f','g','h','j','k','l',';','\'','enter',
+            'shiftleft','z','x','c','v','b','n','m',',','.','.','/','shiftright',
+            'controlleft', 'altleft', 'space', 'altright', 'controlright']
 
 varRow = 2
 varCol = 0
-for key in keys:
+for key in keyboard:
     command = lambda x=key: select(x)
     #special keys
     if key == 'space':
-	    space = Button(keyboardApp, text = 'Space', width = 50, relief = 'ridge', padx = 4, pady = 4, command = command).grid(row = 6, column = 2, columnspan = 7)
+        space = Button(keyboardApp, text = 'Space', width = 50, relief = 'ridge', padx = 4, pady = 4, command = command).grid(row = 6, column = 2, columnspan = 7)
     elif key == 'altright':
         altr = Button(keyboardApp, text = 'rAlt', width = 5, relief = 'ridge', padx = 4, pady = 4, command = command).grid(row = 6, column = 9)
     elif key == 'altleft':
@@ -78,7 +74,7 @@ for key in keys:
         shiftl = Button(keyboardApp, text = 'rSlhift', width = 5, relief = 'ridge', padx = 4, pady = 4, command = command).grid(row = 5, column = 0)
     #normal characters
     else:
-	    notSpecial = Button(keyboardApp, text = key, width = 5, relief = 'ridge', padx = 4, pady = 4, command = command).grid(row = varRow, column = varCol)
+        notSpecial = Button(keyboardApp, text = key, width = 5, relief = 'ridge', padx = 4, pady = 4, command = command).grid(row = varRow, column = varCol)
     
     #this makes sure the 'keyboard' isn't a straight line
     varCol += 1
@@ -91,5 +87,27 @@ for key in keys:
     if varCol > 12 and varRow == 4:
         varCol = 0
         varRow += 1
+
+#keybinds
+def lhold(e):
+    lholding = False
+    if lholding == True:
+        pag.mouseUp(button = 'left')
+    else:
+        pag.mouseDown(button = 'left')
+        lholding = True
+def rhold(e):
+    rholding = False
+    if rholding == True:
+        pag.mouseUp(button = 'right')
+    else:
+        pag.mouseDown(button = 'right')
+        rholding = True
+def kill(e):
+    keyboardApp.destroy()
+
+keyboardApp.bind('<Control-h>', lhold)
+keyboardApp.bind('<Control-j>', rhold)
+keyboardApp.bind('<Control-k>', kill)
 
 keyboardApp.mainloop()
